180d1390f2f6621951662351978d26c70978d5ca
ChenJie <jery1997@foxmail.com>
2018-07-02 00:48:15 +0800

    "3.0中使用vuex和router"

---
+++ src/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>page{{msg}} </h1>
+    <input type="button" @click="incrment">
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      i:1
+    }
+
+  },
+      computed:{
+      msg(){
+return this.$store.state.count;
+      }
+    },
+    methods:{
+      incrment(){
+        this.$store.commit("changeCount",this.i++)
+      }
+    }
+}
+</script>
